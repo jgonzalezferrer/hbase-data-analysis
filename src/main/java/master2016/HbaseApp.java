bed4ed99dbcb9679a63cc1385e506bdcd65fdd02
javiergf14 <antoniojavier.gonzalez.ferrer@alumnos.upm.es>
2016-12-26 13:39:38 -0800

    First commit.

---
+++ src/main/java/master2016/HbaseApp.java
@@ -0,0 +1,13 @@
+package master2016;
+
+/**
+ * Hello world!
+ *
+ */
+public class HbaseApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
